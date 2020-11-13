de52edad5d2e6e8f75f8fcabfce5f29d6802cc9c
L0nfish <fegurka123@gmail.com>
2020-11-13 09:49:53 +0600

    Lab6-8

---
+++ Lab6-8/models/Classroom.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "classroom".
+ *
+ * @property int $classroom_id
+ * @property string $name
+ * @property int $active
+ *
+ * @property Schedule[] $schedules
+ */
+class Classroom extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'classroom';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['active'], 'integer'],
+            [['name'], 'string', 'max' => 20],
+            [['classroom_id'], 'unique', 'targetClass' => Classroom::className(), 'message' => 'Класс успешно добавлен'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'classroom_id' => 'Classroom ID',
+            'name' => 'Name',
+            'active' => 'Active',
+        ];
+    }
+
+    /**
+     * Gets query for [[Schedules]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+  
+
+    /**
+     * Gets query for [[Otdel]].
+     *
+     * @return \yii\db\ActiveQuery|\app\models\queries\OtdelQuery
+     */
+    public function getSchedule()
+    {
+        return $this->hasOne(Schedule::className(), ['classroom_id' => 'classroom_id']);
+    }
+    
+
+    public function loadAndSave($bodyParams)
+    {
+        $classroom = ($this->isNewRecord) ? new Classroom() : Classroom::findOne($this->classroom_id);
+        if ($classroom->load($bodyParams, '') && $classroom->save()) {
+            if ($this->isNewRecord) {
+                $this->classroom_id = $classroom->classroom_id;
+            }
+            if ($this->load($bodyParams, '') && $this->save()) {
+                return true;
+            }
+}
+
+        return false;
+
+    }
+    public function fields()
+    {
+        $fields = parent::fields();
+        return array_merge($fields, [
+            'classroom_id' => function () { return $this->classroom_id;},
+            'name' => function () { return $this->name;},
+            'active' => function () { return $this->active;},
+        ]);
+    }
+    public static function find()
+    {
+        return new \app\models\queries\UserQuery(get_called_class());
+    }
+}
